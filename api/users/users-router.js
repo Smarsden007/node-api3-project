b4245b7a3da06bbb73df62dc8e10179382f9439e
Smarsden007 <seannmarsden@gmail.com>
2022-05-11 23:06:15 -0700

    WIP

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,46 +1,66 @@
 const express = require('express');
 const {
   validateUserId,
+  validateUser,
+  validatePost
 } = require('../middleware/middleware')
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
+
+const User = require('./users-model')
+const post = require('../posts/posts-model')
+
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get('/', (req, res, next) => {
+  User.get()
+  .then(users => {
+    res.json(users)
+  })
+  .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+res.json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
+
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
+
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
+
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
 router.post('/:id/posts', validateUserId, (req, res) => {
+  
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: 'something tragic inside posts router happened',
+    message: err.message,
+    stack:err.stack,
+  })
+})
 
 module.exports = server
